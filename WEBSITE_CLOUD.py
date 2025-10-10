ace743ea9b15b79d3358542f72049427b4140bcd
gpereiraufsj <104570603+gpereiraufsj@users.noreply.github.com>
2025-10-10 16:17:55 -0300

    Update WEBSITE_CLOUD.py

--- WEBSITE_CLOUD.py
+++ WEBSITE_CLOUD.py
@@ -146,7 +146,7 @@ with col_grafico:
 st.subheader("📷 Visualização: RGB | NDVI | Classificação")
 fig_path = os.path.join(output_vis_folder, f"fig_macrofitas_{selected_date}.png")
 if os.path.exists(fig_path):
-    st.image(Image.open(fig_path), use_collumn_width=True)
+    st.image(Image.open(fig_path), use_column_width=True)
 else:
     st.warning(f"Imagem não encontrada: {fig_path}")
 
@@ -169,6 +169,7 @@ st.plotly_chart(fig_comp, use_container_width=True)
 
 st.markdown("---")
 st.caption("Versão científica interativa • Desenvolvido com 💚 para o Projeto AQUASMART")
+
 
 
 
