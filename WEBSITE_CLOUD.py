b87f173082595464369d176df0ae364841a07d2d
gpereiraufsj <104570603+gpereiraufsj@users.noreply.github.com>
2025-10-10 15:53:53 -0300

    Update WEBSITE_CLOUD.py

--- WEBSITE_CLOUD.py
+++ WEBSITE_CLOUD.py
@@ -25,7 +25,7 @@ base_path = pathlib.Path(__file__).parent
 classif_folder = base_path / "saida_SIRGAS2000"
 output_vis_folder = base_path / "output_vis"
 csv_path = classif_folder / "area_macrofitas.csv"
-logo_path = base_path / "Logo.png"
+logo_path = "https://github.com/gpereiraufsj/Macrofitas_Aquasmart/blob/main/Logo.png"
 
 st.set_page_config(layout="wide", page_title="Macrofitas Monitoramento")
 
@@ -155,3 +155,4 @@ st.plotly_chart(fig_comp, use_container_width=True)
 st.markdown("---")
 
 st.caption("Versão científica interativa • Desenvolvido com 💚 para o Projeto AQUASMART")
+
