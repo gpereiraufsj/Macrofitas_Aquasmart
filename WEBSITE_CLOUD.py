1cd4d98b39effb4070b871915c255fe7bea02948
gpereiraufsj <104570603+gpereiraufsj@users.noreply.github.com>
2026-04-24 15:24:29 -0300

    Update WEBSITE_CLOUD.py

--- WEBSITE_CLOUD.py
+++ WEBSITE_CLOUD.py
@@ -55,7 +55,7 @@ with st.sidebar:
             "🌿 Macrófitas",
             "💧 Qualidade da Água",
             "🌎 Uso do Solo & Água",
-            "🛰️ Mudanças MapBiomas",
+            "🛰️ Mudanças LULC",
             "🧱 Sedimentos & Risco",
         ],
         index=0
